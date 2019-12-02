54f4e2b6ec9bfccb470c5b1c381fad07cba22d42
Phan Le Thanh <phanlethanh93@gmail.com>
2019-12-03 00:09:44 +0700

    Get disk space

--- src/controllers/healthController.js
+++ src/controllers/healthController.js
@@ -2,28 +2,36 @@ const response = require('../util/response');
 const os = require('os');
 const healthUtil = require('../util/healthUtil');
 const store = require('../store');
+const checkDiskSpace = require('check-disk-space');
 
 var healthController = {};
 
-healthController.getHealth = function(req, res) {
+healthController.getHealth = async function(req, res) {
     try {
         let uptime = healthUtil.calculateUptime(store.getStartTime());
         let machineUptime = healthUtil.convertSecondToUptime(os.uptime());
 
+        let memory = {}
         let used = -1;
-        let free = -1;
-        let total = -1;
+        memory.free = -1;
+        memory.total = -1;
         used = process.memoryUsage().heapUsed / (1024 * 1024);
-        free = os.freemem() / (1024 * 1024);
-        total = os.totalmem() / (1024 * 1024);
+        memory.free = (os.freemem() / (1024 * 1024)).toFixed(1);
+        memory.total = (os.totalmem() / (1024 * 1024)).toFixed(1);
+
+        let diskPath = os.platform() === 'win32' ? 'C:' : '/';
+        let diskSpace = await checkDiskSpace(diskPath);
+
+        diskSpace.freeGb = (diskSpace.free / (1024 * 1024 * 1024)).toFixed(1); // GB
+        diskSpace.sizeGb = (diskSpace.size / (1024 * 1024 * 1024)).toFixed(1); // GB
 
         let data = {
             status: 'Up',
             uptime: uptime,
             machineUptime: machineUptime,
             memoryUsage: used.toFixed(1),
-            memoryFree: free.toFixed(1),
-            memoryTotal: total.toFixed(1),
+            memory: memory,
+            diskSpace: diskSpace,
             platform: os.platform()
         };
         response.sendSuccess(res, data);
