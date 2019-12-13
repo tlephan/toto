b7487a37475ab0e39003cd3ef680ad7f690d0d7d
Phan Le Thanh <phanlethanh93@gmail.com>
2019-12-13 23:53:18 +0700

    Create UserModel base on NeDB

---
+++ src/models/userModel.js
@@ -0,0 +1,68 @@
+const Datastore = require('nedb');
+const db = new Datastore({ filename: '/opt/toto-db/user.db', autoload: true });
+
+const userModel = {};
+
+userModel.create = async function(data) {
+    let doc = {};
+    return new Promise((resolve, reject) => {
+        db.insert(doc, (err, newDoc) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(newDoc);
+            }
+        });
+    });
+};
+
+userModel.find = async function(query) {
+    return new Promise((resolve, reject) => {
+        db.find(query, function(err, docs) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(docs);
+            }
+        });
+    });
+};
+
+userModel.findOne = async function(id) {
+    let query = {
+        id: id
+    };
+    return new Promise((resolve, reject) => {
+        db.find(query, function(err, docs) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(docs[0]);
+            }
+        });
+    });
+};
+
+userModel.update = async function(query, update) {
+    return new Promise((resolve, reject) => {
+        db.update(query, update, {}, (err, numReplaced) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(numReplaced);
+            }
+        });
+    });
+};
+
+userModel.deleteOne = async function(id) {
+    return new Promise((resolve, reject) => {
+        db.remove({ id: id }, {}, (err, numRemoved) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(numRemoved);
+            }
+        });
+    });
+};
