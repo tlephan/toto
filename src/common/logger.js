0f25cb60a3cc40ce7bc4fbbf261c8a67e8099def
Phan Le Thanh <phanlethanh93@gmail.com>
2019-12-14 19:52:54 +0700

    Create logger

---
+++ src/common/logger.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { createLogger, format, transports } = require('winston');
+const { combine, label, printf } = format;
+require('winston-daily-rotate-file');
+const logging = require('../config/logging.json');
+
+var transport = new transports.DailyRotateFile({
+    dirname: logging.dirname,
+    filename: logging.filename,
+    datePattern: logging.datePattern,
+    zippedArchive: logging.zippedArchive,
+    maxSize: logging.maxSize,
+    maxFiles: logging.maxFiles
+});
+
+transport.on('rotate', function(oldFilename, newFilename) {
+    // Handle rotate event
+});
+
+const customFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} ${level} [${label}] ${message}`;
+});
+
+const logger = function(moduleName) {
+    if (moduleName === null || moduleName.trim() === '') {
+        moduleName = 'unknown_module';
+    }
+    return createLogger({
+        level: logging.level, // Level to log message
+        format: combine(
+            label({ label: moduleName }),
+            format.timestamp({
+                format: 'YYYY-MM-DD HH:mm:ss' // Local time by timezone
+            }),
+            customFormat
+        ),
+        transports: [transport]
+    });
+};
+
+module.exports = logger;
