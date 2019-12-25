e5e523cd5ceec97e30b6b58673a0587931ff422c
Phan Le Thanh <phanlethanh93@gmail.com>
2019-12-25 18:00:41 +0700

    Feature: convert to favicon.ico

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(favicon(path.join(__dirname, 'public', 'images', 'toto_logo_sm.png')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(helmet());
 app.use(compression());
 app.use(cookieParser());
