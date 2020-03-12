658eeec75f9ae7e659c0d817ba9535ae33a802b8
Thanh Phan <phanlethanh93@gmail.com>
2020-03-12 14:10:49 +0700

    Fix: retreive proxy forwarded ip in lower case

--- src/util/requestUtil.js
+++ src/util/requestUtil.js
@@ -2,15 +2,16 @@ const logger = require('../common/logger')('RequestUtil');
 var requestUtil = {};
 
 requestUtil.getRemoteIp = function(req) {
-    //let remoteAddr =
-    //    req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
-    logger.debug(`Request headers: ${JSON.stringify(req.headers, null, 4)}`);
-    let forward =
-        req.headers['X-Forwarded-For'] === undefined
-            ? ''
-            : req.headers['X-Forwarded-For'];
-    let remoteAddr =
-        'Proxy: ' + forward + ' - Header: ' + req.connection.remoteAddress;
+    //logger.debug(`Request headers: ${JSON.stringify(req.headers, null, 4)}`);
+    let remoteAddr = '';
+    if (req.headers['X-Forwarded-For'] !== undefined) {
+        remoteAddr = req.headers['X-Forwarded-For'];
+    } else if (req.headers['x-forwarded-for'] !== undefined) {
+        remoteAddr = req.headers['x-forwarded-for'];
+    } else {
+        remoteAddr = req.connection.remoteAddress;
+    }
+
     if (remoteAddr === '::1') {
         remoteAddr = '127.0.0.1';
     } else {
