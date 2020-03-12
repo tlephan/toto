4de7b8e2ba71e7bb31ef403c835ecfa71bf6d8b7
Thanh Phan <phanlethanh93@gmail.com>
2020-03-12 13:28:22 +0700

    Feature: test remote address retreive

--- src/util/requestUtil.js
+++ src/util/requestUtil.js
@@ -1,8 +1,14 @@
 var requestUtil = {};
 
 requestUtil.getRemoteIp = function(req) {
+    //let remoteAddr =
+    //    req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
+    let forward =
+        req.headers['X-Forwarded-For'] === undefined
+            ? ''
+            : req.headers['X-Forwarded-For'];
     let remoteAddr =
-        req.headers['X-Forwarded-For'] || req.connection.remoteAddress;
+        'Proxy: ' + forward + ' - Header: ' + req.connection.remoteAddress;
     if (remoteAddr === '::1') {
         remoteAddr = '127.0.0.1';
     } else {
